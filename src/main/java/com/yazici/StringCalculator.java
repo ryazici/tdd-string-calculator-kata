f0f207f137052f80116e1fac5b7d48c80e5e6c09
ryazici <rdvnzc@gmail.com>
2018-07-11 19:10:36 +0300

    Renaming & indentations

--- src/main/java/com/yazici/StringCalculator.java
+++ src/main/java/com/yazici/StringCalculator.java
@@ -2,57 +2,53 @@ package com.yazici;
 
 public class StringCalculator {
 
-	public double calculate(String equation,String delimiter) {
+	public double calculate(String equation, String delimiter) {
 
-		equation = NoOperationZeroCases(equation);
+		equation = checkNoOperationZeroCases(equation);
 
-		if(equation == "0")
+		if (equation == "0")
 			return 0;
-		
-		if(equation.contains("-") )
+
+		if (equation.contains("-"))
 			throw new RuntimeException("Numbers must be greater than zero.");
 
-			return sumMultipleSpecialDelimiter(equation);
+		return sumMultipleSpecialDelimiteredNumbers(equation);
 	}
 
-	public String NoOperationZeroCases(String checkEquation){
-		if(checkEquation == null || checkEquation.isEmpty())
-			checkEquation="0";
+	public String checkNoOperationZeroCases(String checkEquation) {
+		if (checkEquation == null || checkEquation.isEmpty())
+			checkEquation = "0";
 		return checkEquation;
 	}
-	
-	public double SumOfDelimitedNumber(String equation,String delimiter) {
-		double result=0.0;
+
+	public double sumOfDelimitedNumber(String equation, String delimiter) {
+		double result = 0.0;
 		String[] numbers = equation.split(delimiter);
-		 for (int i = 0; i < numbers.length; i++) {
-			 if(Double.parseDouble(numbers[i])<=1000)
-			 result += Double.parseDouble(numbers[i]);
+		for (int i = 0; i < numbers.length; i++) {
+			if (Double.parseDouble(numbers[i]) <= 1000)
+				result += Double.parseDouble(numbers[i]);
 		}
 		return result;
 	}
-	
-	
-	
-	public double sumMultipleSpecialDelimiter(String equation) {
-		double result=0;
-		StringBuilder sb= new StringBuilder();
+
+	public double sumMultipleSpecialDelimiteredNumbers(String equation) {
+		double result = 0;
+		StringBuilder sb = new StringBuilder();
 
 		for (int i = 0; i < equation.length(); ++i) {
-			if(Character.isDigit(equation.charAt(i)))
+			if (Character.isDigit(equation.charAt(i)))
 				sb.append(equation.charAt(i));
-			else if( sb.length()>0 && !Character.isDigit(equation.charAt(i)))
-			{
-				if(Double.parseDouble(sb.toString())<=1000)
-				result += Double.parseDouble(sb.toString());
-				sb.setLength(0);;
+			else if (sb.length() > 0 && !Character.isDigit(equation.charAt(i))) {
+				if (Double.parseDouble(sb.toString()) <= 1000)
+					result += Double.parseDouble(sb.toString());
+				sb.setLength(0);
 			}
 		}
-		
-		if(sb.length()>0)
-				result += Double.parseDouble(sb.toString());
-		
-		return  result;
+
+		if (sb.length() > 0)
+			result += Double.parseDouble(sb.toString());
+
+		return result;
 	}
-	
 
 }
